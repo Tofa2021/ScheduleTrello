6ea07dcc7c19221bc8f6c2f0e603767c7acc059d
Anton Latyshev <latyshev.antony@gmail.com>
2025-11-14 23:41:35 +0300

    Rework main menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,23 +17,29 @@
             int subgroupNumber = 1;
             String boardId = "68e6984ec50df586088d6dff";
             String listId = "680ba9eeea5071c6bd6503b6";
+            List<Subject> subjects = ScheduleParser.getSubjects(studentGroup);
 
-            System.out.println("Выберите действие:");
-            System.out.println("1) Создать карточки по расписанию");
-            System.out.println("2) Просмотр пар по предмету");
-            switch (Utils.scanInt()) {
-                case 1:
-                    TrelloService trelloService = new TrelloService();
-                    trelloService.createLabCards(ScheduleParser.getSubjects(studentGroup), subgroupNumber, boardId, listId);
-                    break;
-                case 2:
-                    List<Subject> subjects = ScheduleParser.getSubjects(studentGroup);
-                    Subject subject = Utils.select(subjects.toArray(new Subject[0]));
-                    LessonType lessonType = Utils.select(LessonType.values());
-                    subject.getLessonsByType(lessonType).stream().filter(lesson -> lesson.getNumSubgroup() != 2).forEach(System.out::println);
-                default:
-                    System.out.println("Нет такого пункта");
+            cycle:
+            while(true) {
+                System.out.println("Выберите действие:");
+                System.out.println("1) Создать карточки по расписанию");
+                System.out.println("2) Просмотр пар по предмету");
+                System.out.println("0) Выход");
+                switch (Utils.scanInt()) {
+                    case 1:
+                        TrelloService trelloService = new TrelloService();
+                        trelloService.createLabCards(subjects, subgroupNumber, boardId, listId);
+                        break;
+                    case 2:
+                        Subject subject = Utils.select(subjects.toArray(new Subject[0]));
+                        LessonType lessonType = Utils.select(LessonType.values());
+                        subject.getLessonsByType(lessonType).stream().filter(lesson -> lesson.getNumSubgroup() != 2).forEach(System.out::println);
+                        break;
+                    case 0:
+                        break cycle;
+                        default:
+                        System.out.println("Нет такого пункта");
+                }
             }
-
         }
     }
