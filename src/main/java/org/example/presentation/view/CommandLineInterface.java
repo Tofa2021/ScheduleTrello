dcb74bd39fa8bf5b54c39465465c43a157f7bf71
Anton Latyshev <latyshev.antony@gmail.com>
2026-06-20 14:29:47 +0300

    Add Actions

--- src/main/java/org/example/presentation/view/CommandLineInterface.java
+++ src/main/java/org/example/presentation/view/CommandLineInterface.java
@@ -13,6 +13,7 @@ public class CommandLineInterface implements Interface {
     private final TaskManagerService taskManagerService;
     private final ScheduleService scheduleService;
     private final String groupId;
+    private final String listId;
     private boolean isStopped = false;
 
     public void open() {
@@ -22,34 +23,54 @@ public class CommandLineInterface implements Interface {
 
     private void show() {
         while (!isStopped) {
-            showMenu();
+            showMenuAction();
             switch (Utils.scanInt()) {
-                case 1 -> createScheduleCards();
-                case 2 -> showSubjectLessons();
-                case 3 -> exit();
+                case 1 -> showSubjectLessonsAction();
+                case 2 -> createScheduleCardsAction();
+                case 3 -> createSubjectCardsAction();
+                case 0 -> exit();
                 default -> System.out.println("Нет такого пункта");
             }
         }
     }
 
-    private void showMenu() {
+    private void showMenuAction() {
         System.out.println("Выберите действие:");
-        System.out.println("1) Создать карточки по расписанию");
-        System.out.println("2) Просмотр пар по предмету");
+        System.out.println("1) Просмотр пар по предмету");
+        System.out.println("2) Создать карточки по расписанию");
+        System.out.println("3) Создать карточки по предмету");
         System.out.println("0) Выход");
     }
 
-    private void createScheduleCards() {
-//        taskManagerService.createTasks();
+    private void createScheduleCardsAction() {
+        List<Subject> subjects = scheduleService.getSubjects(groupId);
+        for (Subject subject : subjects) {
+            createSubjectTasks(subject);
+        }
     }
 
-    private void showSubjectLessons() {
+    private void createSubjectCardsAction() {
+        createSubjectTasks(selectSubject());
+    }
+
+    private void createSubjectTasks(Subject subject) {
+        System.out.println("Сколько работ по предмету " + subject.getName());
+        int labCount = Utils.scanBorderInt(0, 20);
+
+        taskManagerService.createSubjectTasks(subject, labCount, listId);
+    }
+
+    private void showSubjectLessonsAction() {
+        Subject subject = selectSubject();
+
+        Utils.printStringList(subject.getLessons().stream().map(Object::toString).toList());
+    }
+
+    private Subject selectSubject() {
         List<String> subjectNames = scheduleService.getSubjectNames(groupId);
         String selectedSubjectName = Utils.select(subjectNames.toArray(String[]::new));
 
-        Subject subject = scheduleService.getSubject(selectedSubjectName, groupId);
-
-        Utils.printStringList(subject.getLessons().stream().map(Object::toString).toList());
+        return scheduleService.getSubject(selectedSubjectName, groupId);
     }
 
     private void exit() {
