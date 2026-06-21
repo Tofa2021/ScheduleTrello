b65012afc1c5491f4419963ba9c83a6710250a6e
Anton Latyshev <latyshev.antony@gmail.com>
2026-06-21 13:37:45 +0300

    Add multiSelect

--- src/main/java/org/example/util/scanner/ScannerManager.java
+++ src/main/java/org/example/util/scanner/ScannerManager.java
@@ -2,6 +2,7 @@ package org.example.util.scanner;
 
 import org.example.model.Nameable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -31,6 +32,19 @@ public interface ScannerManager {
         }
     }
 
+    default boolean scanBoolean() {
+        while (true) {
+            String input = scanString();
+            if (InputPatterns.FALSE_BOOLEAN.match(input)) {
+                return false;
+            }
+            if (InputPatterns.TRUE_BOOLEAN.match(input)) {
+                return true;
+            }
+            System.out.println("Не удалось понять согласие или отрицание");
+        }
+    }
+
     default <T extends Nameable> T select(T[] values) {
         printStringList(Arrays.stream(values)
                 .map(T::getName)
@@ -53,28 +67,67 @@ public interface ScannerManager {
         return values[chosenIndex];
     }
 
+    default <T extends Nameable> List<T> multiSelect(T[] values, int selectionNumber) {
+        if (values.length < selectionNumber) {
+            throw new IllegalArgumentException("Selection number cannot be bigger that values length");
+        }
+
+        List<T> result = new ArrayList<>();
+        List<T> cloned = new ArrayList<>(List.of(values));
+        while (selectionNumber-- > 0) {
+            printStringList(cloned);
+            T selected = select(cloned);
+            result.add(selected);
+            cloned.remove(selected);
+        }
+
+        return result;
+    }
+
+    default <T extends Nameable> List<T> multiSelect(T[] values) {
+        System.out.println("Для завершения выбора нажмите ENTER");
+
+        List<T> copiedElements = new ArrayList<>(Arrays.asList(values));
+        List<T> selectedElements = new ArrayList<>();
+
+        while (!copiedElements.isEmpty()) {
+            printStringList(copiedElements);
+            String input = scanString();
+            if (input.isEmpty()) {
+                break;
+            }
+
+            try {
+                int index = Integer.parseInt(input) - 1;
+                if (index < 0 || index >= copiedElements.size()) {
+                    System.out.println("Число выходит за границу диапазона. Повторите попытку");
+                    continue;
+                }
+
+                T element = copiedElements.get(index);
+                copiedElements.remove(index);
+                selectedElements.add(element);
+            } catch (InputMismatchException | NumberFormatException e) {
+                System.out.println("Ввод должны быть числом");
+            }
+        }
+
+        return selectedElements;
+    }
+
     default <T> void printStringList(List<T> values) {
         printStringList(values.stream().map(Object::toString).toArray(String[]::new));
     }
 
     default void printStringList(String[] values) {
+        if (values.length == 0) {
+            System.out.println("Пусто");
+        }
+
         for (int i = 0; i < values.length; i++) {
             System.out.print(i + 1);
             System.out.print(") ");
             System.out.println(values[i]);
-        }
-    }
-
-    default boolean scanBoolean() {
-        while (true) {
-            String input = scanString();
-            if (InputPatterns.FALSE_BOOLEAN.match(input)) {
-                return false;
-            }
-            if (InputPatterns.TRUE_BOOLEAN.match(input)) {
-                return true;
-            }
-            System.out.println("Не удалось понять согласие или отрицание");
         }
     }
 }
